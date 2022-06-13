19b70f5b29689fb0a868f5aaf7fb7c4f45516e9e
Antonio Alfonso <antonioalfonsomartinez255@gmail.com>
2022-06-13 08:26:02 +0200

    Se creó el while del programa y se definieron las 2 primeros comandos: 'cd' (función 'change_dir()' y 'q'.

--- file_manager.py
+++ file_manager.py
@@ -1,2 +1,27 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import os
-from colorama import Fore, Back
+#from colorama import init, Fore, Back, Style
+#import re
+
+def change_dir(d):
+    if os.path.isdir(d):
+        os.chdir(d)
+        print("Current Directory: {}".format(os.getcwd()))
+    else:
+        print("Directory not found.")
+        
+
+commands = ['cd','q']
+#start()
+
+while True:
+    command = input(">>> ").split(" ")
+    if command[0] in commands:
+        if command[0] == 'cd':
+            change_dir(command[1])
+        elif command[0] == 'q':
+            break
+    else:
+        print("INVALID COMMAND")
