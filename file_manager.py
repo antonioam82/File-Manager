50ae225f81536ef4536c53aa93be401e91fedb53
Antonio Alfonso <antonioalfonsomartinez255@gmail.com>
2022-06-14 18:23:29 +0200

    Se definió la función de inicio 'start()' y se cambión el puntero del input. También se introdujo el uso de 'colorama'.

--- file_manager.py
+++ file_manager.py
@@ -1,9 +1,8 @@
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-#from colorama import init, Fore, Back, Style
-#import re
+from colorama import init, Fore, Back, Style
+import re
 
 def change_dir(d):
     if os.path.isdir(d):
@@ -11,13 +10,18 @@ def change_dir(d):
         print("Current Directory: {}".format(os.getcwd()))
     else:
         print("Directory not found.")
+
+def start():
+    init()
+    print(Back.BLUE+"\n------------------------------FILE MANAGER------------------------------"+Back.RESET+"\n")
+    print("Directorio actual: {} ".format(os.getcwd())+"\n")
         
 
 commands = ['cd','q']
-#start()
+start()
 
 while True:
-    command = input(">>> ").split(" ")
+    command = input("c:\> ").split(" ")
     if command[0] in commands:
         if command[0] == 'cd':
             change_dir(command[1])
